48d2519feb84ead59e15aa16541ab549e1f267d5
ek275 <ek275@IT490F.myguest.virtualbox.org>
2023-10-26 19:10:30 -0400

    db files

--- db/php/dbFunction.php
+++ db/php/dbFunction.php
@@ -7,10 +7,32 @@
     require_once('rabbitMQClient.php');
     require_once('dbConnection.php');
 
+//Login function
+    function doLogin($username, $passwd){
+        
+        $connection = dbConnection();
+        
+        $query = "SELECT * FROM Users WHERE username = '$username'";
+        $result = $connection->query($query);
+        if($result){
+            if($result->num_rows == 0){
+                return false;
+            }else{
+                while ($row = $result->fetch_assoc()){
+                    $salt = $row['salt']; 
+                    $password = hashPassword($passwd, $salt);
+                    if ($row['password'] == $password){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }
+            }
+        }
+    }
 
-
-//  This function registers a new user 
-    function register($username, $password){
+//  Registration function 
+    function register($username, $passwd){
         
         //Makes connection to database
         $connection = dbConnection();
@@ -19,12 +41,39 @@
         $salt = randomString(29);
         
         //Hashes password
-        $h_password = hashPassword($password, $salt);
+        $password = hashPassword($passwd, $salt);
         
         //Query for a new user
-        $newuser_query = "INSERT INTO user VALUES ('$username', '$email', '$h_password', '$salt', '$firstname', '$lastname')";
+        $newuser_query = "INSERT INTO Users VALUES ('$username', '$password', '$salt')";
         
         $result = $connection->query($newuser_query);
         
         return true;
     }
+
+
+//Hash function
+    function hashPassword($passwd, $salt){
+            $new_pass = $passwd . $salt;
+             return hash("sha256", $new_pass);
+        }
+
+//Random string salt function
+   function randomString($length) {
+            $randstr = '';
+            srand((double) microtime(TRUE) * 1000000);
+
+            $chars = array(
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p',
+                'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5',
+                '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+                'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+
+            for ($rand = 0; $rand <= $length; $rand++) {
+                $random = rand(0, count($chars) - 1);
+                $randstr .= $chars[$random];
+            }
+            return $randstr;
+   }
+
+
