da21b4504c581c7944a245fd31d032844b4e41f7
ek275 <ek275@IT490F.myguest.virtualbox.org>
2023-10-30 20:14:06 -0400

    editing db files

--- db/php/dbFunction.php
+++ db/php/dbFunction.php
@@ -7,9 +7,74 @@
     require_once('rabbitMQClient.php');
     require_once('dbConnection.php');
 
+// Login Function
+    function doLogin($username, $password){
+        
+        $connection = dbConnection();
+        
+        $query = "SELECT * FROM Users WHERE username = '$username'";
+        $result = $connection->query($query);
+        if($result){
+            if($result->num_rows == 0){
+                return false;
+            }else{
+                while ($row = $result->fetch_assoc()){
+                    $salt = $row['salt']; 
+                    $h_password = hashPassword($password, $salt);
+                    if ($row['h_password'] == $h_password){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }
+            }
+        }
+    }
 
+ // Check username availibility function
+    function checkUsername($username){
+        
+        $connection = dbConnection();
+        
+        //Query to check if the username is taken
+        $check_username = "SELECT * FROM Users WHERE username = '$username'";
+        $check_result = $connection->query($check_username);
+        
+        if($check_result){
+            if($check_result->num_rows == 0){
+                return true;
+            }elseif($check_result->num_rows == 1){
+                return false;
+                }
+        }
+    }
+   
+   // Generates random string for salt
+   function randomString($length) {
+            $randstr = '';
+            srand((double) microtime(TRUE) * 1000000);
+           
+            $chars = array(
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p',
+                'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5',
+                '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 
+                'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
 
-//  This function registers a new user 
+            for ($rand = 0; $rand <= $length; $rand++) {
+                $random = rand(0, count($chars) - 1);
+                $randstr .= $chars[$random];
+            }
+            return $randstr;
+    }
+    
+    // Hashes password for storing
+    function hashPassword($password, $salt){
+            $new_pass = $password . $salt;
+             return hash("ek275", $new_pass);
+        }
+ 
+
+// Register function 
     function register($username, $password){
         
         //Makes connection to database
